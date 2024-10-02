c7319b47aa103e2fda650b53e574fbfe911943ae
marko <marko.jagetic3@gmail.com>
2024-10-02 09:40:13 +0200

    Add pages layer logic

--- src/app/Router.tsx
+++ src/app/Router.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import { AppDrawer } from '../widgets/app-drawer'
-import { Explore } from '../pages/explore/ui'
+import { Explore } from '../pages/explore'
+import { Login } from '../pages/login'
 
 export const Routing = () => {
     return (
@@ -11,6 +12,7 @@ export const Routing = () => {
                 <div>
                     <Routes>
                         <Route path="/" element={<Explore />} />
+                        <Route path="/login" element={<Login />} />
                     </Routes>
                 </div>
             </Router>
